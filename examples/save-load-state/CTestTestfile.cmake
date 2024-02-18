36ce22fa57d7268702a6f57c2551bbceacc3de26
nma5214 <nawafmalansari@gmail.com>
2024-02-18 17:15:15 -0500

    Fixing baby llama issue related to graph allocation with ggml_context

---
+++ examples/save-load-state/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nma5214/.vscodeProjects/clones/llama.cpp/examples/save-load-state
+# Build directory: /home/nma5214/.vscodeProjects/clones/llama.cpp/examples/save-load-state
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
